2375b565557f8615cfae755ef19f6c5801e096fc
jpe <patriot.game@gmail.com>
2021-12-07 02:48:56 -0500

    memoize calculation

--- src/day7.clj
+++ src/day7.clj
@@ -1,44 +1,26 @@
 (ns day7
   (:require [aocd.core :as data]
             [clojure.string :as str]
-            [clojure.math.numeric-tower :refer [abs]]))
-
-(def input (data/input 2021 7))
+            [clojure.math.numeric-tower :refer [abs]]
+            [clojure.pprint :refer [cl-format]]))
 
 (defn parse-input [input]
   (as-> input i
-        (str/trim i)
-        (str/split i #",")
-        (map #(Integer/parseInt %) i)))
-
-(defn distance-from [alignment]
-  (fn [potential-position]
-    (abs (- alignment potential-position))))
-
-(def parsed-input (parse-input input))
-
-(defn input-range-for [input]
-  (range (apply min input) (apply max input)))
-
-(defn calculate-fuel [distance]
-  (apply + (take distance (iterate inc 1))))
+    (str/trim i) (str/split i #",") (map #(Integer/parseInt %) i)))
 
 (defn sum-of-distances [number in]
   (->> in
-       (map (distance-from number))
-       (apply +)))
+       (map #(abs (- % number))) (apply +)))
 
+(def calculate-fuel (memoize (fn [d] (apply + (take d (iterate inc 1))))))
 (defn sum-of-distances-2 [number in]
   (->> in
-       (map (distance-from number))
-       (map calculate-fuel)
-       (apply +)))
+       (map #(abs (- % number))) (map calculate-fuel) (apply +)))
 
 (defn solve [f input]
-  (as-> (input-range-for input) i
-        (map #(f % input) i)
-        (apply min i)))
+  (as-> (range (apply min input) (apply max input)) i
+        (map #(f % input) i) (apply min i)))
 
-(let [parsed-input (parse-input input)]
-  (solve sum-of-distances parsed-input)
-  (solve sum-of-distances-2 parsed-input))
+(let [parsed-input (-> (data/input 2021 7) parse-input)]
+  (cl-format true "Part 1: ~d~%" (solve sum-of-distances parsed-input))
+  (cl-format true "Part 2: ~d~%" (solve sum-of-distances-2 parsed-input)))
